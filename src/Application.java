39c33d2fc59c67d0008a228cce4cd7fbf64ec48a
Pedro Ferreira <up201806506@fe.up.pt>
2020-02-20 20:00:39 +0000

    Added sum

--- src/Application.java
+++ src/Application.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String operation = scanner.nextLine();
 
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(a + b);
+        if (operation.equals("sum"))
+        {
+            System.out.println(a + b);
+        }
+
+
     }
 }
