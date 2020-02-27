d5dabf056c4c8243ad53178af9464db20ce4ac68
Paulinho <up201806505@fe.up.pt>
2020-02-27 18:37:18 +0000

    Creating the Game

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,21 +7,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) {
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-
-            screen.setCursorPosition(null);   // we don't need a cursor
-            screen.startScreen();             // screens must be started
-            screen.doResizeIfNecessary();     // resize screen if necessary
-
-            screen.clear();
-            screen.setCharacter(10, 10, new TextCharacter('X'));
-            screen.refresh();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        Game jogo = new Game();
+        jogo.run();
     }
 }
