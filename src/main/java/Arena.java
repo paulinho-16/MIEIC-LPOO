ab0aa2a55a728854c5dd468398958a5453b7666f
Paulinho <up201806505@fe.up.pt>
2020-02-28 22:29:31 +0000

    Coins

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -8,18 +8,21 @@ import com.googlecode.lanterna.screen.Screen;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Arena {
     private int width;
     private int height;
     Hero hero;
     private List<Wall> walls;
+    private List<Coin> coins;
 
     Arena(int width, int height) {
         this.width = width;
         this.height = height;
         hero = new Hero(10, 10);
         walls = createWalls();
+        coins = createCoins();
     }
 
     private List<Wall> createWalls() {
@@ -40,12 +43,33 @@ public class Arena {
         return walls;
     }
 
+    private List<Coin> createCoins() {
+        Random random = new Random();
+        ArrayList<Coin> coins = new ArrayList<>();
+        while (coins.size() < 5) {
+            boolean encontrado = false;
+            Coin coin = new Coin(random.nextInt(width - 2) + 1, random.nextInt(height - 2) + 1);
+            for (Coin moeda : coins) {
+                if (moeda.position.equals(coin.position)) {
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (coin.position.equals(hero.position) || encontrado)
+                continue;
+            coins.add(coin);
+        }
+        return coins;
+    }
+
     public void draw(TextGraphics graphics) throws IOException {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0,0), new TerminalSize(width, height), ' ');
         for (Wall wall : walls)
             wall.draw(graphics);
         hero.draw(graphics);
+        for (Coin coin : coins)
+            coin.draw(graphics);
     }
 
     private boolean canHeroMove(Position position) {
@@ -61,9 +85,19 @@ public class Arena {
         return false;
     }
 
+    public void retrieveCoins() {
+        for (Coin coin : coins) {
+            if (coin.position.equals(hero.position)) {
+                coins.remove(coin);
+                break;
+            }
+        }
+    }
+
     public void moveHero(Position position) {
         if (canHeroMove(position))
             hero.setPosition(position);
+        retrieveCoins();
     }
 
     public void processKey(KeyStroke key) {
