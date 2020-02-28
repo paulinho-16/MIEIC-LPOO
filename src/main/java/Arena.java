83051af4a2b5555621f66ddbb87ceca438777cad
Paulinho <up201806505@fe.up.pt>
2020-02-28 10:46:39 +0000

    Walls

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -6,21 +6,45 @@ import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
     private int width;
     private int height;
     Hero hero;
+    private List<Wall> walls;
 
     Arena(int width, int height) {
         this.width = width;
         this.height = height;
         hero = new Hero(10, 10);
+        walls = createWalls();
+    }
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+            walls.add(new Wall(1, r));
+            walls.add(new Wall(width - 2, r));
+        }
+
+        return walls;
     }
 
     public void draw(TextGraphics graphics) throws IOException {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0,0), new TerminalSize(width, height), ' ');
+        for (Wall wall : walls)
+            wall.draw(graphics);
         hero.draw(graphics);
     }
 
---
+++ src/main/java/Wall.java
@@ -0,0 +1,20 @@
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Wall {
+    private Position position;
+
+    Wall(int x, int y) {
+        position = new Position(x,y);
+    }
+
+    public void setPosition(Position position) {
+        this.position.setX(position.getX());
+        this.position.setY(position.getY());
+    }
+
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#0a0580"));
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), String.valueOf(Symbols.BLOCK_SOLID));
+    }
+}
