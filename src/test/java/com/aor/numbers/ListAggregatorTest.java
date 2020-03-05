a88a6a9ac1f8497196e8001cf6bd39755a2bd9fb
Paulinho <up201806505@fe.up.pt>
2020-03-05 18:11:39 +0000

    Initial commit

---
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -0,0 +1,87 @@
+package com.aor.numbers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ListAggregatorTest {
+
+    List<Integer> list = new ArrayList<>();
+
+    /*@Before
+    public void helper() {
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(2);
+        list.add(5);
+    }*/
+
+    @Test
+    public void sum() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(2);
+        list.add(5);
+
+        ListAggregator aggregator = new ListAggregator(list);
+
+        int sum = aggregator.sum();
+
+        assertEquals(14, sum);
+    }
+
+    @Test
+    public void max() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(2);
+        list.add(5);
+
+        ListAggregator aggregator = new ListAggregator(list);
+
+        int max = aggregator.max();
+
+        assertEquals(5, max);
+    }
+
+    @Test
+    public void min() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(2);
+        list.add(5);
+
+        ListAggregator aggregator = new ListAggregator(list);
+
+        int min = aggregator.min();
+
+        assertEquals(1, min);
+    }
+
+    @Test
+    public void distinct() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(2);
+        list.add(5);
+
+        ListAggregator aggregator = new ListAggregator(list);
+
+        int distinct = aggregator.distinct();
+
+        assertEquals(4, distinct);
+    }
+}
