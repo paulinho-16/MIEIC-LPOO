2fb263ee725284c038093dff6d03f3f66c988f8b
Paulinho <up201806505@fe.up.pt>
2020-03-26 19:14:39 +0000

    Observer Pattern

---
+++ src/main/java/StringBar.java
@@ -0,0 +1,24 @@
+public class StringBar extends Bar {
+    private boolean happyHour;
+
+    public StringBar() {
+        happyHour = false;
+    }
+
+    @Override
+    public boolean isHappyHour() {
+        return happyHour;
+    }
+
+    @Override
+    public void startHappyHour() {
+        happyHour = true;
+        notifyObservers();
+    }
+
+    @Override
+    public void endHappyHour() {
+        happyHour = false;
+        notifyObservers();
+    }
+}
